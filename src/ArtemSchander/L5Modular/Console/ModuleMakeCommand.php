258f1b3cf2b7953aee7f0b8230b2f5305ee6bfe4
Farhan Wazir <farhanwazir@users.noreply.github.com>
2015-07-09 05:43:18 +0400

    Add transaction
    Add transaction directory and example file.

--- src/ArtemSchander/L5Modular/Console/ModuleMakeCommand.php
+++ src/ArtemSchander/L5Modular/Console/ModuleMakeCommand.php
@@ -54,6 +54,9 @@ class ModuleMakeCommand extends GeneratorCommand {
 
 		// Create Views folder
 		$this->generate('view');
+		
+		// Create Translations folder
+		$this->generate('translation');
 
 		// Create Routes file
 		$this->generate('routes');
@@ -86,6 +89,10 @@ class ModuleMakeCommand extends GeneratorCommand {
 			case 'view':
 				$filename = 'index.blade';
 				break;
+				
+			case 'translation':
+				$filename = 'test';
+				break;
 			
 			case 'routes':
 				$filename = 'routes';
@@ -93,14 +100,14 @@ class ModuleMakeCommand extends GeneratorCommand {
 		}
 
 		// $suffix = ($type == 'controller') ? ucfirst($type) : '';
-		$folder = ($type != 'routes') ? ucfirst($type).'s\\' : '';
+		$folder = ($type != 'routes') ? ucfirst($type).'s\\'. ($type === 'translation' ? 'en\\':'') : '';
 
 		$name = $this->parseName('Modules\\'.$this->getNameInput().'\\'.$folder.$filename);
 		if ($this->files->exists($path = $this->getPath($name))) 
 			return $this->error($this->type.' already exists!');
 
 		$this->currentStub = __DIR__.'/stubs/'.$type.'.stub';
-
+		
 		$this->makeDirectory($path);
 		$this->files->put($path, $this->buildClass($name));
 	}
