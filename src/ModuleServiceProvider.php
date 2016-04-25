d0b8a1ac8f16b96206014092997871df487d0a82
Thomas Eriksson <thomas@mosquitodesign.net>
2016-04-25 14:03:10 +0200

    Solution for route caching

--- src/ModuleServiceProvider.php
+++ src/ModuleServiceProvider.php
@@ -15,16 +15,17 @@ class ModuleServiceProvider extends ServiceProvider {
 	public function boot() {
 
 		if(is_dir(app_path().'/Modules/')) {
-
 			$modules = config("modules.enable") ?: array_map('class_basename', $this->files->directories(app_path().'/Modules/'));
 			foreach($modules as $module)  {
-				
-				$routes = app_path().'/Modules/'.$module.'/routes.php';
+				// Allow routes to be cached
+				if (!$this->app->routesAreCached()) {
+					$routes = app_path() . '/Modules/' . $module . '/routes.php';
+					if($this->files->exists($routes)) include $routes;
+				}
 				$helper = app_path().'/Modules/'.$module.'/helper.php';
 				$views  = app_path().'/Modules/'.$module.'/Views';
 				$trans  = app_path().'/Modules/'.$module.'/Translations';
 
-				if($this->files->exists($routes)) include $routes;
 				if($this->files->exists($helper)) include $helper;
 				if($this->files->isDirectory($views)) $this->loadViewsFrom($views, $module);
 				if($this->files->isDirectory($trans)) $this->loadTranslationsFrom($trans, $module);
