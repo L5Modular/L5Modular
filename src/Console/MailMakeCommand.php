a13e5d7d198d72db2f00109e0224b60fdb53b794
Artem Schander <a.schander@vanameland.de>
2020-03-21 12:12:19 +0100

    fix mail stubs

--- src/Console/MailMakeCommand.php
+++ src/Console/MailMakeCommand.php
@@ -43,16 +43,42 @@ class MailMakeCommand extends BaseMailMakeCommand
      */
     protected function writeMarkdownTemplate()
     {
-        $path = app_path() . '/Modules/' . Str::studly($this->module) . '/' . $this->getConfiguredFolder('views') . '/' . str_replace('.', '/', $this->option('markdown')) . '.blade.php';
+		$path = $this->getConfiguredFolder('views');
+        $file = str_replace('.', '/', $this->option('markdown')) . '.blade.php';
 
-        if (!$this->files->isDirectory(dirname($path))) {
-            $this->files->makeDirectory(dirname($path), 0755, true);
+        $file = str_replace('//', '/', app_path("Modules/{$this->module}/{$path}/{$file}"));
+
+        if (! $this->files->isDirectory(dirname($file))) {
+            $this->files->makeDirectory(dirname($file), 0755, true);
         }
 
         $base_class = new \ReflectionClass(BaseMailMakeCommand::class);
 
         $base_class_path = dirname($base_class->getFileName());
 
-        $this->files->put($path, file_get_contents($base_class_path . '/stubs/markdown.stub'));
+        $this->files->put($file, file_get_contents($base_class_path . '/stubs/markdown.stub'));
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        return str_replace('DummyModuleName', $this->module, parent::buildClass($name));
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return $this->option('markdown')
+                        ? __DIR__.'/stubs/markdown-mail.stub'
+                        : __DIR__.'/stubs/mail.stub';
     }
 }
